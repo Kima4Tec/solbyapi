d4ffaa40f3b4cf62a9fff3f41e648cfe9e82c332
Kim Lasse Massesson <kima4@elev.tec.dk>
2026-01-15 10:17:59 +0100

    WithComments

--- Test/MovieControllerTests.cs
+++ Test/MovieControllerTests.cs
@@ -1,52 +1,68 @@
-﻿using Xunit;
-using NSubstitute;
-using Application.Interfaces;
-using Application.Dtos.Movies;
-using Microsoft.AspNetCore.Mvc;
-using API.Controllers;
+﻿using Xunit;                       // Bruges til test-framework (xUnit)
+using NSubstitute;                  // Bruges til at mocke interfaces og services
+using Application.Interfaces;       // Her ligger IMovieService
+using Application.Dtos.Movies;      // DTO'er til movies
+using Microsoft.AspNetCore.Mvc;     // Bruges til ActionResult-typer (OkObjectResult, NotFoundResult)
+using API.Controllers;              // Controller-klassen vi tester
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 public class MoviesControllerTests
 {
+    // Mock af IMovieService vha. NSubstitute
     private readonly IMovieService _movieService = Substitute.For<IMovieService>();
+
+    // Controller vi vil teste
     private readonly MoviesController _controller;
 
+    // Constructor: vi initialiserer controlleren med den mockede service
     public MoviesControllerTests()
     {
         _controller = new MoviesController(_movieService);
     }
 
-    [Fact]
+    // Test af GET /api/movies
+    [Fact]  // xUnit attribut, markerer en test-metode
     public async Task Get_ShouldReturnAllMovies()
     {
-        // Arrange
+        // Arrange: forbered testdata og hvad mocken skal returnere
         var movies = new List<MovieReadDto>
         {
             new MovieReadDto { Id = Guid.NewGuid(), Title = "Movie 1" },
             new MovieReadDto { Id = Guid.NewGuid(), Title = "Movie 2" }
         };
 
+        // Mock: Når GetMoviesAsync() kaldes, returner testlisten
         _movieService.GetMoviesAsync().Returns(movies);
 
-        // Act
+        // Act: udfør selve handlingen
         var result = await _controller.Get();
 
-        // Assert
+        // Assert: tjek at vi fik et OkObjectResult
         var okResult = Assert.IsType<OkObjectResult>(result.Result);
+
+        // Assert: tjek at resultatet indeholder en liste af MovieReadDto
         var returnedMovies = Assert.IsAssignableFrom<List<MovieReadDto>>(okResult.Value);
+
+        // Assert: tjek at der er præcis 2 elementer
         Assert.Equal(2, returnedMovies.Count);
     }
 
+    // Test af GET /api/movies/{id} hvor filmen ikke findes
     [Fact]
     public async Task GetById_ShouldReturnNotFound_WhenMissing()
     {
+        // Arrange: en tilfældig id som ikke findes
         var id = Guid.NewGuid();
+
+        // Mock: GetMovieAsync returnerer null for denne id
         _movieService.GetMovieAsync(id).Returns((MovieReadDto?)null);
 
+        // Act: kald controller-metoden
         var result = await _controller.Get(id);
 
+        // Assert: tjek at resultatet er NotFoundResult
         Assert.IsType<NotFoundResult>(result.Result);
     }
 }
